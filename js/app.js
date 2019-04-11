a9ea06d8c2d2afac555324f6bcd84701c46926eb
Matthew Krieger <CSgrad2017@gmail.com>
2019-04-11 16:23:55 -0400

    almost

--- js/app.js
+++ js/app.js
@@ -21,13 +21,12 @@ function showSlides() {
 
 
 
-
 let app = {};
 
 
 $('#searchButton').click(function(e){
 	e.preventDefault();
-	$('#tvShows').empty();
+	$('#tvMaze').empty();
 	let word = $('#searchBox').val();
 	app.getTVshow(word);
 });
@@ -54,260 +53,73 @@ app.getTVshow = function(inputWord){
 
 app.displayTV = function(tvInfoArray){
 
-	let count;
+	let count; // declare a let to hold the shows number
 	if (count == undefined) {
-		$('#tvShows').text('No show was found. Please do a new search.').css('color', '#FF0000');
+		$('.showResult').text('No show was found. Please do a new search.').css('color', 'red');
 	};
 
+	// using jQuery forEach to loop over our array of tv information
 	tvInfoArray.forEach(function(tvInfo){
 
+		// Creating elements for each piece of data
 		let name = $('<h2>').text(tvInfo.show.name);
-		let nameBox = $('<h3>').text(tvInfo.show.name);
+		let nameBox = $('<h3>').text(tvInfo.show.name).css({"border-bottom": "2px solid red"});
 		let language = $('<i>').text(tvInfo.show.language);
-		// let genres = '';
-		// for (let i = 0; i< tvInfo.show.genres.length; i++) {
-		// 	genres += $('<p>').text(tvInfo.show.genres[i]);
-		// }
 		let genres = $('<p>').text(tvInfo.show.genres[0]);
-		let genres2 = $('<p>').text(tvInfo.show.genres[2]);
-		let genres1 = $('<p>').text(tvInfo.show.genres[1]);
-
 		let channel = $('<p>').text(tvInfo.show.network.name);
 		let country = $('<p>').text('( ' + tvInfo.show.network.country.name + ' )');
-		let image = $('<img>').attr('src', tvInfo.show.image.original);
+		let image = $('<img>').attr('src', tvInfo.show.image.original).css({"height": "300px", "width": "200px"});
 		let imageBox = $('<img>').attr('src', tvInfo.show.image.original);
 		let summary = tvInfo.show.summary;
 
+		// make images clickable
 		let clkImage = $('<a>').attr('href', tvInfo.show.image.original).append(image);
 
+		// appending in divs only name and image for the search
 		let shows = $('<div>').addClass('shows').append(name, clkImage);
 
-		$('#tvShows').append(shows);
+		// appending all the divs to our webpage
+		$('#tvMaze').append(shows);
 
+		// counting every div and displey the result
 		count = $('.shows').length;
-		$('.showResult').text('You have ' + count + ' result.').css('color', 'purple');
-
+		$('.showResult').text('You have ' + count + ' result.').css('color', 'green');
+		if (count > 12){
+			$('#search').css('height', '2000px');
+		}else if (count > 8 && count <= 12) {
+			$('#search').css('height', '1800px');
+		}else if (count > 4 && count <= 8) {
+			$('#search').css('height', '1200px');
+		}else{
+			$('#search').css('height', '900px');
+		}
+		$('header').hide();
+		// appending in divs all data for the lightBox
 		let showsInfo = $('<div>').addClass('showsInfo').append(imageBox, nameBox, language, genres, channel, country, summary);
 
+		// creating lightBox by clicking the shows
 		$('.shows').click(function(e){
 			e.preventDefault();
-
-			if ($('#lightBox').length > 0) {
+			//check if lightBox exist and declare let to hold the lightBox
+			if ($('#lightBox').length > 0) {          	// .lightbox exists
 				$('#lightBox').show();
-			} else {
+			} else { 									// .lightbox does not exist
 				let lightBox = 	'<div id="lightBox">' +
 					'<p title="close" id="lightBoxClose">X</p>' +
 					'<div class="showsBox">' + '</div>' +
 					'</div>';
 
-				$('body').append(lightBox);
-				$('.showsBox').append(showsInfo);
+				$('body').append(lightBox);	            //insert lightbox into page
+				$('.showsBox').append(showsInfo);		//insert divs into lightBox
 			};
 		});
 
+		// remove lightBox when click "X"
 		$('#lightBoxClose').live('click', function() {
+
 			$('#lightBox').remove();
+			$('header').show();
 		});
 
-
-	});
-};
-
-
-
-
-
-
-
-// $(() => {
-// 	let currentImgIndex = 0;
-// 	let highestIndex = $('.carousel-images').children().length - 1;
-// 	// next button
-// 	$('.next').click(function() {
-// 		// hide current image
-// 		$('.carousel-images').children().eq(currentImgIndex).css('display', 'none');
-// 		// increment image index
-// 		if (currentImgIndex < highestIndex) {
-// 			currentImgIndex++;
-// 		}
-// 		else {
-// 			currentImgIndex = 0;
-// 		}
-//
-// 		// show current image
-// 		$('.carousel-images').children().eq(currentImgIndex).css('display', 'block');
-// 	});
-//
-// 	$('.previous').click(function() {
-// 		// hide current image
-// 		$('.carousel-images').children().eq(currentImgIndex).css('display', 'none');
-// 		// decrement the image index
-// 		if (currentImgIndex > 0) {
-// 			currentImgIndex--;
-// 		}
-// 		else {
-// 			currentImgIndex = highestIndex;
-// 		}
-// 		// show current image
-// 		$('.carousel-images').children().eq(currentImgIndex).css('display', 'block');
-//
-// 	});
-// });
-
-
-
-// let slideIndex = 0;
-// showSlides();
-//
-// function showSlides() {
-// 	let i;
-// 	let slides = document.getElementsByClassName("mySlides");
-// 	let dots = document.getElementsByClassName("dot");
-// 	for (i = 0; i < slides.length; i++) {
-// 		slides[i].style.display = "none";
-// 	}
-// 	slideIndex++;
-// 	if (slideIndex > slides.length) {slideIndex = 1}
-// 	for (i = 0; i < dots.length; i++) {
-// 		dots[i].className = dots[i].className.replace(" active", "");
-// 	}
-// 	slides[slideIndex-1].style.display = "block";
-// 	dots[slideIndex-1].className += " active";
-// 	setTimeout(showSlides, 5000);
-// }
-//
-//
-//
-//
-//
-// let app = {};
-//
-//
-// $('#searchButton').click(function(e){
-// 	e.preventDefault();
-// 	$('#tvShows').empty();
-// 	let word = $('#searchBox').val();
-// 	app.getTVshow(word);
-// });
-//
-// app.getTVshow = function(inputWord){
-// 	$.ajax({
-// 		url: 'http://api.tvmaze.com/search/shows',
-// 		method: 'GET',
-// 		dataType: 'json',
-// 		data: {
-// 			format: 'json',
-// 			q: inputWord
-// 		},
-// 		success: function(result){
-// 			console.log('Ajax is working.');
-// 			app.displayTV(result);
-// 		},
-// 		error: function(error){
-// 			console.log('Something went wrong.');
-// 			console.log(error);
-// 		}
-// 	});
-// };
-//
-// app.displayTV = function(tvInfoArray){
-//
-// 	let count;
-// 	if (count == undefined) {
-// 		$('#tvShows').text('No show was found. Please do a new search.').css('color', '#FF0000');
-// 	};
-//
-// 	tvInfoArray.forEach(function(tvInfo){
-//
-// 		let name = $('<h2>').text(tvInfo.show.name);
-// 		let nameBox = $('<h3>').text(tvInfo.show.name);
-// 		let language = $('<i>').text(tvInfo.show.language);
-// 		// let genres = '';
-// 		// for (let i = 0; i< tvInfo.show.genres.length; i++) {
-// 		// 	genres += $('<p>').text(tvInfo.show.genres[i]);
-// 		// }
-// 		let genres = $('<p>').text(tvInfo.show.genres[0]);
-// 		let genres2 = $('<p>').text(tvInfo.show.genres[2]);
-// 		let genres1 = $('<p>').text(tvInfo.show.genres[1]);
-//
-// 		let channel = $('<p>').text(tvInfo.show.network.name);
-// 		let country = $('<p>').text('( ' + tvInfo.show.network.country.name + ' )');
-// 		let image = $('<img>').attr('src', tvInfo.show.image.original);
-// 		let imageBox = $('<img>').attr('src', tvInfo.show.image.original);
-// 		let summary = tvInfo.show.summary;
-//
-// 		let clkImage = $('<a>').attr('href', tvInfo.show.image.original).append(image);
-//
-// 		let shows = $('<div>').addClass('shows').append(name, clkImage);
-//
-// 		$('#tvShows').append(shows);
-//
-// 		count = $('.shows').length;
-// 		$('#tvShows').text('You have ' + count + ' result.').css('color', 'purple');
-//
-// 		let showsInfo = $('<div>').addClass('showsInfo').append(imageBox, nameBox, language, genres, channel, country, summary);
-//
-// 		$('.shows').click(function(e){
-// 			e.preventDefault();
-//
-// 			if ($('#lightBox').length > 0) {
-// 				$('#lightBox').show();
-// 			} else {
-// 				let lightBox = 	'<div id="lightBox">' +
-// 					'<p title="close" id="lightBoxClose">X</p>' +
-// 					'<div class="showsBox">' + '</div>' +
-// 					'</div>';
-//
-// 				$('body').append(lightBox);
-// 				$('.showsBox').append(showsInfo);
-// 			};
-// 		});
-//
-// 		$('#lightBoxClose').live('click', function() {
-// 			$('#lightBox').remove();
-// 		});
-//
-//
-// 	});
-// };
-//
-//
-//
-//
-//
-//
-//
-// // $(() => {
-// // 	let currentImgIndex = 0;
-// // 	let highestIndex = $('.carousel-images').children().length - 1;
-// // 	// next button
-// // 	$('.next').click(function() {
-// // 		// hide current image
-// // 		$('.carousel-images').children().eq(currentImgIndex).css('display', 'none');
-// // 		// increment image index
-// // 		if (currentImgIndex < highestIndex) {
-// // 			currentImgIndex++;
-// // 		}
-// // 		else {
-// // 			currentImgIndex = 0;
-// // 		}
-// //
-// // 		// show current image
-// // 		$('.carousel-images').children().eq(currentImgIndex).css('display', 'block');
-// // 	});
-// //
-// // 	$('.previous').click(function() {
-// // 		// hide current image
-// // 		$('.carousel-images').children().eq(currentImgIndex).css('display', 'none');
-// // 		// decrement the image index
-// // 		if (currentImgIndex > 0) {
-// // 			currentImgIndex--;
-// // 		}
-// // 		else {
-// // 			currentImgIndex = highestIndex;
-// // 		}
-// // 		// show current image
-// // 		$('.carousel-images').children().eq(currentImgIndex).css('display', 'block');
-// //
-// // 	});
-// // });
+	});  // end forEach loop function
+}; //end app.displayTV function
